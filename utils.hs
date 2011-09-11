806849c125099ba1d19a5304226baf4e05ad4443
Aditya Bhargava <batman@scribd.com>
2011-09-11 16:00:55 -0700

    adding glob, regex, join and a bunch of other functions

--- utils.hs
+++ utils.hs
@@ -4,8 +4,36 @@ import System.Info (os)
 import System.Process (readProcessWithExitCode)
 import System.Exit
 import Text.Printf
+import Control.Monad (forM)
+import System.Directory (doesDirectoryExist, getDirectoryContents)
+import System.FilePath ((</>))
 import qualified Data.List as L
+import Data.List.Split
 import qualified Data.Text as T
+import Text.Regex
+
+-- convenience functions to translate Maybes to bools
+maybe_bool (Just _)= True
+maybe_bool Nothing = False
+
+-- convenience function to make Haskell regexes behave like Perl regexes
+str =~ regex = maybe_bool $ matchRegex (mkRegex regex) str
+str !~ regex = not $ str =~ regex
+
+joinOn str l = foldl1 (\acc x -> acc ++ str ++ x) l
+
+join = joinOn ""
+
+-- given a file name, get the name of the directory that it's in
+dirName f = ((joinOn "/").init.(splitOn "/")) f
+
+-- a glob function. usage:
+-- glob "/usr/local/bin/*.hs"
+glob pat = do
+    -- try to guess the directory name by ignoring everything after the first * and getting the dir name from the part before the *.
+    let dir = dirName (fst (break (=='*') pat))
+    contents <- getRecursiveContents dir
+    return $ filter (=~ pat) contents
 
 uppercase = map T.toUpper
 lowercase = map T.toLower
@@ -27,8 +55,23 @@ openBrowser u = trybrowsers browsers u
                | os=="mingw32" = ["c:/Program Files/Mozilla Firefox/firefox.exe"]
                | otherwise     = ["sensible-browser","gnome-www-browser","firefox"]
 
+
+-- Given a directory, get all the contents in that dir, recursively.
+getRecursiveContents :: FilePath -> IO [FilePath]
+getRecursiveContents topdir = do
+  names <- getDirectoryContents topdir
+  let properNames = filter (`notElem` [".", ".."]) names
+  paths <- forM properNames $ \name -> do
+    let path = topdir </> name
+    isDirectory <- doesDirectoryExist path
+    if isDirectory
+      then getRecursiveContents path
+      else return [path]
+  return (concat paths)
+
+
 {-
 main = do
     openBrowser "http://google.com"
-
+    getRecursiveContents "."
 -}
